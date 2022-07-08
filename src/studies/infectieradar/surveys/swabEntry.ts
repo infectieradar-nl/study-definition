b98ccd2d5f130dc6a7c83af28ec9717e2f99bcc5
Infectieradar <62208136+Infectieradar@users.noreply.github.com>
2022-07-08 15:27:35 +0200

    Last bits

--- src/studies/infectieradar/surveys/swabEntry.ts
+++ src/studies/infectieradar/surveys/swabEntry.ts
@@ -20,16 +20,16 @@ class SwabEntryDef extends SurveyDefinition {
     super({
       surveyKey: surveyKeys.swabEntry,
       name: new Map([
-        ["en", "Weakly questionnaire "],
-        ["nl", "Wekelijkse vragenlijst"],
+        ["en", "Register for Testing in Infectieradar"],
+        ["nl", "Aanmelden voor Testen in Infectieradar"],
       ]),
       description: new Map([
-        ["en", "Survey about your health status in the last week."],
-        ["nl", "Klik hier voor je vragenlijst over je klachten in de afgelopen week. Meld alsjeblieft ook als je geen klachten had."],
+        ["en", "Clcik here to participate in selftests"],
+        ["nl", "Klik hier om je aan te melden voor het ontvangen van zelftesten."],
       ]),
       durationText: new Map([
-        ["en", "15 seconds to 3 minutes, depending on your symptoms."],
-        ["nl", "Invullen duurt 15 seconden tot 3 minuten, afhankelijk van je klachten."],
+        ["en", "Fill in your personal details so we can send you the tests, and you can see testresults."],
+        ["nl", "Laat je gegevens achter zodat we je de testen kunnen toesturen, en je je uitslag kan inzien."],
       ])
     });
 
