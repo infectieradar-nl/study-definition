34ca9846bbc658ffafbcae24858600b07f50e681
Infectieradar <62208136+Infectieradar@users.noreply.github.com>
2022-08-24 13:47:15 +0200

    wat kleine aanpassingen

--- src/studies/infectieradar/surveys/swabEntry.ts
+++ src/studies/infectieradar/surveys/swabEntry.ts
@@ -25,15 +25,15 @@ class SwabEntryDef extends SurveyDefinition {
       surveyKey: surveyKeys.SwabEntry,
       name: new Map([
         ["en", "Register for Testing in Infectieradar"],
-        ["nl", "Aanmelden voor Testen in Infectieradar"],
+        ["nl", "Aanmelden voor het zelftest-onderzoek in Infectieradar"],
       ]),
       description: new Map([
         ["en", "Clcik here to participate in selftests"],
-        ["nl", "Klik hier om je aan te melden voor het ontvangen van zelftesten."],
+        ["nl", "Klik hier om deel te nemen het zelftest-onderzoek."],
       ]),
       durationText: new Map([
         ["en", "Fill in your personal details so we can send you the tests, and you can see testresults."],
-        ["nl", "Laat je gegevens achter zodat we je de testen kunnen toesturen, en je je uitslag kan inzien."],
+        ["nl", "Lees meer over het zelftest-onderzoek onder Informatie. Laat eventueel hier je gegevens achter zodat we je testen kunnen toesturen, en je je uitslag kan inzien."],
       ])
     });
 
@@ -83,10 +83,8 @@ class Intro extends Item {
   }
 
   markdownContent = `
-## Testen met Infectieradar
-Om Corona beter in de gaten te houden en meer te weten welke andere ziekteverwekkers ook klachten veroorzaken vragen we een geselecteerde groep deelnemers coronazelftesten uit te voeren. Soms vragen we ook om een neus- en keelmonster. Dit neus- en keelmonster wordt in het laboratorium onderzocht voor corona (ook het variant) en andere ziekteverwekkers (bijvoorbeeld griep of een ander coronavirus).
-Deelnemers aan dit aanvullend onderzoek zijn uitgenodigd per brief en ontvangen gratis coronazelftesten en krijgen een uitslag van de neus- en keeltesten.
-
+## Zelftest-onderzoek
+Met dit formulier kan je jezelf registreren als deelnemer voor het zelftest-onderzoek. Meer informatie over dit onderzoek kan je [*hier*](/about-selfswabstudy) vinden.
 `
 
   buildItem(): SurveySingleItem {
@@ -114,8 +112,8 @@ class Infos extends Item {
 
   markdownContent = `
 ## Naam, adres, e-mail en 06-nummer
-Voor het versturen en ontvangen van testen gebruikt het RIVM een [zelftestportaal](https://rivmportal.glean.nl/user/auth). Dit is een aparte website waar je extra testen kunt bestellen en je uitslag kan inzien.
-Dit [zelftestportaal](https://rivmportal.glean.nl/user/auth) staat los van de Infectieradarwebsite en maakt gebruik van een inlogprocedure met e-mail en SMS.
+Voor het versturen en ontvangen van testen gebruikt het RIVM het portaal [GLEAN](https://rivmportal.glean.nl/). Dit is een aparte website waar je extra testen kunt bestellen en je uitslag kan inzien.
+Dit [portaal](https://rivmportal.glean.nl/) staat los van de Infectieradarwebsite en maakt gebruik van een inlogprocedure met e-mail en SMS.
 Daarom vragen we hieronder je naam/adres/e-mail en 06-nummer achter te laten. De testen ontvang je dan automatisch in enkele weken.
 `
 
@@ -193,7 +191,7 @@ class Consent extends Item {
       isRequired: this.isRequired,
       condition: this.condition,
       questionText: new Map([
-        ['nl', 'Aanvullende toestemming'],
+        ['nl', 'Aanvullende toestemming zelftest-onderzoek'],
       ]),
       checkBoxLabel: new Map([
         ["nl", "Toestemming geven"],
@@ -202,7 +200,7 @@ class Consent extends Item {
         ComponentGenerators.markdown({
           content: new Map([
             ["nl", `
-Om testen te ontvangen moeten we toestemming hebben om je adresgegevens te verwerken. Ook hebben we je toestemming nodig om de neus- en keelmonsters die je instuurt te testen in het laboratorium en op te slaan.
+Om deel te nemen aan het [zelftest-onderzoek](/about-selfteststudy) moeten we toestemming hebben om je adresgegevens te verwerken. Ook hebben we je toestemming nodig om de neus- en keelmonsters die je instuurt te testen in het laboratorium en op te slaan.
 Vink hieronder "Toestemming geven" aan om de toestemmingsverklaring te lezen.
 `]]),
         })
@@ -214,18 +212,18 @@ Vink hieronder "Toestemming geven" aan om de toestemmingsverklaring te lezen.
         ["nl", `
 **Scroll naar beneden om de hele tekst te lezen, geef onderaan wel of geen toestemming.**
 
-Door de knop “ik geef toestemming” aan te klikken stem je in met deelname aan het aanvullende testonderzoek van Infectieradar en ga je akkoord dat het RIVM je gegevens voor dit onderzoek zal verwerken .
+Door de knop “ik geef toestemming” aan te klikken stem je in met deelname aan het [zelftest-onderzoek van Infectieradar](/about-selfteststudy) en ga je akkoord dat het RIVM je gegevens voor dit onderzoek zal verwerken.
 
-Het onderzoek is gericht op het in kaart brengen van gezondheidsklachten die kunnen wijzen op infectieziekten, zoals het coronavirus. Dit onderzoek is van belang om na te gaan of het aantal mensen met klachten toe- of afneemt en om de verspreiding van infectieziekten in de gaten te houden. Meer informatie over onder andere het doel van het onderzoek en je rechten kun je vinden in de [privacyverklaring van Infectieradar](/privacy) en de [RIVM privacyverklaring](https://www.rivm.nl/sites/default/files/2018-11/RIVM%20%20Privacyverklaring%20mei%202018%20definitief%20Nederlands.pdf).
+Het onderzoek is gericht op het in kaart brengen van gezondheidsklachten die kunnen wijzen op infectieziekten, zoals het coronavirus (COVID-19). Dit onderzoek is van belang om na te gaan of het aantal mensen met klachten toe- of afneemt en om de verspreiding van infectieziekten in de gaten te houden. Meer informatie over onder andere het doel van het onderzoek en je rechten kun je vinden in de [privacyverklaring van Infectieradar](/privacy).
 
 Ook:
--	Heb ik de informatie op de website van Infectieradar over het onderzoek en de [RIVM privacyverklaring](https://www.rivm.nl/sites/default/files/2018-11/RIVM%20%20Privacyverklaring%20mei%202018%20definitief%20Nederlands.pdf) over de verwerking van de persoonsgegevens door het RIVM goed gelezen en begrepen.
+-	Heb ik de informatie op de website van Infectieradar over het [onderzoek](/about-selfteststudy) en de [privacyverklaring](/privacy) over de verwerking van de persoonsgegevens door het RIVM goed gelezen en begrepen.
 -	Heb ik goed over mijn deelname aan het onderzoek kunnen nadenken.
--	Weet ik dat meedoen aan het onderzoek vrijwillig is. Ik weet ook dat ik op ieder moment, zonder opgaaf van een reden, kan stoppen met deelname aan het onderzoek. Ik begrijp dat het intrekken van mijn toestemming geen gevolgen heeft voor de verwerking van mijn persoonsgegevens in de periode voorafgaand aan het intrekken van mijn toestemming. Na het stopzetten van de deelname zijn alleen nog mijn onderzoeksgegevens en eventueel ingezonden neus- en keelmonsters voor het onderzoek beschikbaar, de onderzoeksgegevens en monsters zijn dan echter niet meer te herleiden tot mij als persoon. Daarnaast gelden de bewaartermijnen, zoals opgegeven in de privacyverklaring, voor mijn overige persoonsgegevens.
--	Weet ik dat de onderzoeksgegevens van mijn eventuele neus- en keelmonster(s) tot 15 jaar na ontvangst bewaard kan worden en later nog voor een ander onderzoek gebruikt kan worden. En weet ik dat mijn contactgegevens en accountgegevens 2 jaar na de laatste inlog op de websites van het RIVM zelftestportaal en mijn onderzoeksgegevens tot 30 jaar na het invullen van de vragenlijsten worden bewaard en dat onderzoeksgegevens kunnen worden gedeeld met de deelnemers aan het samenwerkingsverband Influenzanet. De onderzoeksresultaten hebben geen bewaartermijn en kunnen dus gepubliceerd blijven.
--	Weet ik dat voor het onderzoek mijn accountgegevens (e-mailadres en wachtwoord), mijn contactgegevens (naam, adres, woonplaats, postcode, telefoonnummer), mijn onderzoeksgegevens (de ingevulde vragenlijsten; met daarin onder andere de eerste 4 cijfers van mijn postcode, mijn geboortejaar en maand, gegevens over mijn gezondheid en zelftest- en zelfswab-uitslagen) en mijn eventuele neus- en keelmonster(s) worden verwerkt.
--	Weet ik dat voor het versturen van nieuwe zelftesten en neus- en keelmonster(s) mijn accountgegevens (e-mailadres en wachtwoord) en mijn contactgegevens (naam, adres, woonplaats, postcode, telefoonnummer) worden gebruikt.
--	Weet ik dat voor het versturen van de testuitslag van mijn neus- en keelmonster(s) mijn accountgegevens (e-mailadres) en mijn contactgegevens (naam, telefoonnummer) worden gebruikt.
+-	Weet ik dat meedoen aan het onderzoek vrijwillig is. Ik weet ook dat ik op ieder moment, zonder opgaaf van een reden, kan stoppen met deelname aan het onderzoek. Ik begrijp dat het intrekken van mijn toestemming geen gevolgen heeft voor de verwerking van mijn persoonsgegevens in de periode voorafgaand aan het intrekken van mijn toestemming. Na het stopzetten van de deelname zijn alleen nog mijn onderzoeksgegevens en eventueel ingezonden neus- en keelmonsters voor het onderzoek beschikbaar, de onderzoeksgegevens en monsters zijn dan echter niet meer te herleiden tot mij als persoon. Daarnaast gelden de bewaartermijnen, zoals opgegeven in de [privacyverklaring](/privacy), voor mijn overige persoonsgegevens.
+-	Weet ik dat de onderzoeksgegevens van mijn eventuele neus- en keelmonster(s) tot 15 jaar na ontvangst bewaard kan worden en later nog voor een ander onderzoek kunnen worden gebruikt. En weet ik dat mijn contactgegevens en accountgegevens 2 jaar na de laatste inlog op Infectieradar of het (zelftest)portaal [GLEAN](https://rivmportal.glean.nl/) en mijn onderzoeksgegevens tot 15 jaar na het invullen van de vragenlijsten worden bewaard en dat onderzoeksgegevens kunnen worden gedeeld met de deelnemers aan het samenwerkingsverband Influenzanet. De onderzoeksresultaten hebben geen bewaartermijn en kunnen dus gepubliceerd blijven.
+-	Weet ik dat voor het onderzoek mijn accountgegevens (e-mailadres en wachtwoord), mijn contactgegevens (naam, adres, woonplaats, postcode, telefoonnummer), mijn onderzoeksgegevens (de ingevulde vragenlijsten; met daarin onder andere de eerste 4 cijfers van mijn postcode, mijn geboortejaar en maand, gegevens over mijn gezondheid en zelftest- en neus- en keelmonster-testuitslagen) en mijn eventuele neus- en keelmonster(s) worden verwerkt.
+-	Weet ik dat voor het versturen van nieuwe zelftest- en neus- en keelmonster-afnamepakketjes mijn contactgegevens (naam, adres, woonplaats, postcode, telefoonnummer) worden gebruikt.
+-	Weet ik dat voor het versturen van de testuitslag van mijn neus- en keelmonster mijn accountgegevens (e-mailadres) en mijn contactgegevens (telefoonnummer) worden gebruikt.
 -	Verklaar ik dat ik 16 jaar of ouder ben.
         `]]),
       acceptBtn: new Map([
@@ -435,7 +433,7 @@ class Telephone extends Item {
       condition: this.condition,
       isRequired: this.isRequired,
       questionText: new Map([[
-        'nl', 'Wat is je 06-nummer? (gebruik de internationale notatie: +316nummer)'
+        'nl', 'Wat is je 06-nummer? (gebruik de internationale notatie, met +31 en zonder 0: +316nummer)'
       ]]),
       confidentialMode: "replace",
       placeholderText: new Map([['nl', '+316########']]),
