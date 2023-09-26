8f962ad5fa782785b37f7828c4db3eb9536f833a
phev8 <phev8@users.noreply.github.com>
2023-09-26 16:50:05 +0200

    run add sex only for active participants if not set yet

--- src/studies/infectieradar/customRules/addSexFlag.ts
+++ src/studies/infectieradar/customRules/addSexFlag.ts
@@ -6,23 +6,33 @@ import { ParticipantFlags } from "../participantFlags";
 export const updateSexFlag_rules = {
   name: "updateSexFlag",
   rules: [
-    StudyEngine.if(
-      StudyEngine.checkConditionForOldResponses(StudyEngine.singleChoice.any(Intake.QGender.key, "1"), "any", Intake.key),
-      StudyEngine.participantActions.updateFlag(
-        ParticipantFlags.gender.key,
-        ParticipantFlags.gender.values.female
+    StudyEngine.ifThen(
+      StudyEngine.and(
+        StudyEngine.participantState.hasStudyStatus('active'),
+        StudyEngine.not(
+          StudyEngine.participantState.hasParticipantFlagKey(
+            ParticipantFlags.gender.key
+          )
+        )
       ),
-      // else
       StudyEngine.if(
-        StudyEngine.checkConditionForOldResponses(StudyEngine.singleChoice.any(Intake.QGender.key, "0"), "any", Intake.key),
+        StudyEngine.checkConditionForOldResponses(StudyEngine.singleChoice.any(Intake.QGender.key, "1"), "any", Intake.key),
         StudyEngine.participantActions.updateFlag(
           ParticipantFlags.gender.key,
-          ParticipantFlags.gender.values.male
+          ParticipantFlags.gender.values.female
         ),
         // else
-        StudyEngine.participantActions.updateFlag(
-          ParticipantFlags.gender.key,
-          ParticipantFlags.gender.values.other
+        StudyEngine.if(
+          StudyEngine.checkConditionForOldResponses(StudyEngine.singleChoice.any(Intake.QGender.key, "0"), "any", Intake.key),
+          StudyEngine.participantActions.updateFlag(
+            ParticipantFlags.gender.key,
+            ParticipantFlags.gender.values.male
+          ),
+          // else
+          StudyEngine.participantActions.updateFlag(
+            ParticipantFlags.gender.key,
+            ParticipantFlags.gender.values.other
+          )
         )
       )
     )
