8c8b740ffb3a065a1093ab7a8f94bb34168d22c2
simeondebruijn <simeon.de.bruijn@rivm.nl>
2023-10-09 12:49:51 +0200

    assign stopcontinue / catchup

---
+++ src/studies/longcovid/customRules/assignStopContinue.ts
@@ -0,0 +1,44 @@
+import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
+
+const flagKey = 'GetCatchup';
+
+const timestampFromStudyStart = (daysDelta: number) => {
+  return StudyEngine.timestampWithOffset(
+    {
+      days: daysDelta
+    },
+    StudyEngine.participantState.getStudyEntryTime(),
+  );
+}
+
+const assignSurveyFromStudyStart = (
+  surveyKey: string,
+  category: "prio" | "normal" | "optional",
+  startDays: number, durationDays: number
+) => {
+  return StudyEngine.participantActions.assignedSurveys.add(surveyKey, category, timestampFromStudyStart(startDays), timestampFromStudyStart(startDays + durationDays));
+}
+
+
+export const assignT15Retroactively_rules = {
+  name: "assignCatchupRetroactively",
+  rules: [
+    StudyEngine.ifThen(
+      // IF has flag catch-up
+      StudyEngine.participantState.hasParticipantFlagKey(flagKey),
+      // ASSIGN Catch-up from study start: //we want to let it stay open for quite some time
+      assignSurveyFromStudyStart('Tstopcontinue', "prio", 360, 360),
+      // FLAG PARTICIPANT TO BE ABLE TO FIND THEM LATER IF NEEDED:
+      StudyEngine.participantActions.updateFlag(
+        'catchupassigned',
+        StudyEngine.timestampWithOffset({ days: 0 })
+      )
+    )
+
+  ]
+}
+
+
+
+
+
---
+++ src/studies/longcovid/customRules/flagCathupParticipants.ts
@@ -0,0 +1,81 @@
+import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
+
+
+// Assing the flag Get_Catch_up to participants that have T9, but not T12 
+export const flagcatchupParticipants_rules = {
+    name: "flagcatchupParticipants",
+    rules: [
+        // Condition 1, participant answerd yes on T12.DEM.extend_FU
+        StudyEngine.if(
+            
+             // If The participant missed a qeustionaire:
+             StudyEngine.and(
+             StudyEngine.participantState.hasParticipantFlagKey('expired')
+             ),
+ 
+ 
+             // can we also use studyexpression.checkSurveyResponsekey(surveykeys, T9)
+             //StudyEngine.and(StudyEngine.participantState.hasParticipantFlagKey('T9')),
+             StudyEngine.and(
+             StudyEngine.checkSurveyResponseKey('T0')),
+
+
+            StudyEngine.not(
+                StudyEngine.checkConditionForOldResponses(
+                StudyEngine.singleChoice.any(
+                  'T12.DEM.extend_FU', 'ja'
+                ),
+                'any', 'T12'
+              ),
+            )
+
+
+    
+
+        ),
+            // Then:
+            StudyEngine.do(StudyEngine.participantActions.updateFlag('noCatchup','GetCatchup'),
+            )
+    ]
+}
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// is this needed for us????
+export const cleanUpT0FromcatchupParticipants_rules = {
+    name: "cleanUpT0FromcatchupParticipants",
+    rules: [
+        StudyEngine.if(
+            StudyEngine.participantState.hasParticipantFlagKeyAndValue('debug', 'shouldHaveFinished_A'),
+            StudyEngine.do(
+                StudyEngine.participantActions.updateFlag('surveyCategory', 'A'),
+                StudyEngine.participantActions.updateFlag("exitStatus", "finished"),
+                StudyEngine.participantActions.assignedSurveys.removeAll(),
+                StudyEngine.participantActions.updateStudyStatus('active'),
+            )
+        )
+    ]
+}
