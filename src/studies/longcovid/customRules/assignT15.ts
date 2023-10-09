0a9c9c14550634cee5ca855da2f886b931d08f9b
merlinvanloenen <merlin.van.loenen@rivm.nl>
2023-10-09 11:46:45 +0200

    assign-catchup
    Draft 1: 9-10-2023
    Merlin - Siméon
    
    First attempt to assign catch-up qeustions,

--- src/studies/longcovid/customRules/assignT15.ts
+++ src/studies/longcovid/customRules/assignT15.ts
@@ -1,6 +1,6 @@
 import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
 
-const flagKey = 'T15fixApplied';
+const flagKey = 'GetCatchup';
 
 const timestampFromStudyStart = (daysDelta: number) => {
   return StudyEngine.timestampWithOffset(
@@ -21,31 +21,24 @@ const assignSurveyFromStudyStart = (
 
 
 export const assignT15Retroactively_rules = {
-  name: "assignT15Retroactively",
+  name: "assignCatchupRetroactively",
   rules: [
     StudyEngine.ifThen(
-      // IF extendFU == ja
-      StudyEngine.and(
-        StudyEngine.checkConditionForOldResponses(
-          StudyEngine.singleChoice.any(
-            'T12.DEM.extend_FU', 'ja'
-          ),
-          'any', 'T12'
-        ),
-        StudyEngine.not(StudyEngine.participantState.hasParticipantFlagKey(flagKey))
-      ),
-      // MAKE SURE WE HAVE ONLY ONE T15:
-      StudyEngine.participantActions.assignedSurveys.remove('T15', 'all'),
-      // ASSIGN T15 from study start:
-      assignSurveyFromStudyStart('T15', "prio", 450, 42),
-      // REMOVE exitFlag
-      StudyEngine.participantActions.removeFlag('exitStatus'),
+      // IF has flag catch-up
+      StudyEngine.if(StudyEngine.participantState.hasParticipantFlagKey(flagKey)),
+      // ASSIGN Catch-up from study start: //we want to let it stay open for quite some time
+      assignSurveyFromStudyStart('Tstopcontinue', "prio", 360, 360),
       // FLAG PARTICIPANT TO BE ABLE TO FIND THEM LATER IF NEEDED:
       StudyEngine.participantActions.updateFlag(
-        flagKey,
+        'catchupassigned',
         StudyEngine.timestampWithOffset({ days: 0 })
       )
     )
 
   ]
 }
+
+
+
+
+
--- src/studies/longcovid/customRules/flagFinishedParticipants.ts
+++ src/studies/longcovid/customRules/flagFinishedParticipants.ts
@@ -1,51 +1,72 @@
 import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
 
 
-export const flagFinishedParticipants_rules = {
-    name: "flagFinishedParticipants",
+// Assing the flag Get_Catch_up to participants that have T9, but not T12 
+export const flagcatchupParticipants_rules = {
+    name: "flagcatchupParticipants",
     rules: [
+        // Condition 1, participant answerd yes on T12.DEM.extend_FU
         StudyEngine.if(
-            // If:
-            StudyEngine.and(
+            
+             // If The participant missed a qeustionaire:
+             StudyEngine.and(
+             StudyEngine(StudyEngine.participantState.hasParticipantFlagKey('expired')
+             )),
+ 
+ 
+             // can we also use studyexpression.checkSurveyResponsekey(surveykeys, T9)
+             //StudyEngine.and(StudyEngine.participantState.hasParticipantFlagKey('T9')),
+             StudyEngine.and(
+             StudyEngine.participantActions.checkSurveyResponsekey('T0')),
+
+
+            StudyEngine.not(
                 StudyEngine.checkConditionForOldResponses(
-                    StudyEngine.singleChoice.any('T12.TEST.Q11', 'nee', 'ja', 'notanymore'),
-                    "any", "T12"
+                StudyEngine.singleChoice.any(
+                  'T12.DEM.extend_FU', 'ja'
                 ),
-                StudyEngine.not(
-                    StudyEngine.checkConditionForOldResponses(
-                        StudyEngine.singleChoice.any('T12.DEM.extend_FU', 'ja'),
-                        "any", "T12"
-                    ),
-                )
-            ),
-            // Then:
-            StudyEngine.do(
-                StudyEngine.participantActions.updateFlag('debug', 'shouldHaveFinished_A'),
+                'any', 'T12'
+              ),
             )
+
+
+    
+
         ),
-        StudyEngine.if(
-            StudyEngine.and(
-                StudyEngine.checkConditionForOldResponses(
-                    StudyEngine.singleChoice.any('T12c.TEST.Q11', 'nee', 'ja', 'notanymore'),
-                    "any", "T12c"
-                ),
-                StudyEngine.not(
-                    StudyEngine.checkConditionForOldResponses(
-                        StudyEngine.singleChoice.any('T12c.DEM.extend_FU', 'ja'),
-                        "any", "T12c"
-                    ),
-                )
-            ),
             // Then:
-            StudyEngine.do(
-                StudyEngine.participantActions.updateFlag('debug', 'shouldHaveFinished_C'),
+            StudyEngine.do(StudyEngine.participantActions.updateFlag('GetCatchup'),
             )
-        )
     ]
 }
 
-export const cleanUpT0FromFinishedParticipants_rules = {
-    name: "cleanUpT0FromFinishedParticipants",
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// is this needed for us????
+export const cleanUpT0FromcatchupParticipants_rules = {
+    name: "cleanUpT0FromcatchupParticipants",
     rules: [
         StudyEngine.if(
             StudyEngine.participantState.hasParticipantFlagKeyAndValue('debug', 'shouldHaveFinished_A'),
