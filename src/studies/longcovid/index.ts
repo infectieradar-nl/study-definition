38fad8197b130f36b63b35073296507d388aaf4a
simeondebruijn <simeon.de.bruijn@rivm.nl>
2023-10-09 13:07:18 +0200

    fixes and cleaning for assign stopcontinue

--- src/studies/longcovid/index.ts
+++ src/studies/longcovid/index.ts
@@ -1,7 +1,10 @@
 import { Study } from "case-editor-tools/types/study";
 import { assignT15Retroactively_rules } from "./customRules/assignT15";
+import { assignCatchupRetroactively_rules } from "./customRules/assignStopContinue";
 import { flagFinishedParticipants_rules, cleanUpT0FromFinishedParticipants_rules } from "./customRules/flagFinishedParticipants";
 import { inviteToExternalStudy_rules } from "./customRules/inviteToExternalStudy";
+import { flagcatchupParticipants_rules } from "./customRules/flagCathupParticipants";
+
 
 export const LongCovidStudy: Study = {
   studyKey: 'longcovid',
@@ -14,5 +17,7 @@ export const LongCovidStudy: Study = {
     cleanUpT0FromFinishedParticipants_rules,
     inviteToExternalStudy_rules,
     assignT15Retroactively_rules,
+    assignCatchupRetroactively_rules,
+    flagcatchupParticipants_rules
   ]
 }
