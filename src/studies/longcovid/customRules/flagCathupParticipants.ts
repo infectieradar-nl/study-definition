7832dc404f2814769fe6f2633c7a4b95c2375a51
phev8 <phev8@users.noreply.github.com>
2023-10-11 10:46:35 +0200

    update catchup flag script

--- src/studies/longcovid/customRules/flagCathupParticipants.ts
+++ src/studies/longcovid/customRules/flagCathupParticipants.ts
@@ -1,40 +1,36 @@
 import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
 
 
-// Assing the flag Get_Catch_up to participants that have T9, but not T12 
+// Assing the flag Get_Catch_up to participants that have T9, but not T12
 export const flagcatchupParticipants_rules = {
-    name: "flagcatchupParticipants",
-    rules: [
-        // Condition 1, participant answerd yes on T12.DEM.extend_FU
-        StudyEngine.if(
-            
-             // If The participant missed a qeustionaire:
-             StudyEngine.and(
-             StudyEngine.participantState.hasParticipantFlagKey('expired')
-             ),
- 
- 
-             // can we also use studyexpression.checkSurveyResponsekey(surveykeys, T9)
-             //StudyEngine.and(StudyEngine.participantState.hasParticipantFlagKey('T9')),
-             StudyEngine.and(
-             StudyEngine.checkSurveyResponseKey('T0')),
-
-
-            StudyEngine.not(
-                StudyEngine.checkConditionForOldResponses(
-                StudyEngine.singleChoice.any(
-                  'T12.DEM.extend_FU', 'ja'
-                ),
-                'any', 'T12'
-              ),
-            )
-
-
-    
-
+  name: "flagcatchupParticipants",
+  rules: [
+    StudyEngine.if(
+      // condition:
+      StudyEngine.and(
+        // If The participant missed a qeustionaire:
+        StudyEngine.participantState.hasParticipantFlagKey('expired'),
+        // If they started the study:
+        StudyEngine.participantState.lastSubmissionDateOlderThan(
+          StudyEngine.timestampWithOffset({ days: 0 }),
+          'T0'
         ),
-            // Then:
-            StudyEngine.do(StudyEngine.participantActions.updateFlag('noCatchup','GetCatchup'),
-            )
-    ]
+        // If participant answerd yes on T12.DEM.extend_FU
+        StudyEngine.not(
+          StudyEngine.checkConditionForOldResponses(
+            StudyEngine.singleChoice.any(
+              'T12.DEM.extend_FU', 'ja'
+            ),
+            'any', 'T12'
+          ),
+        )
+      ),
+      // then:
+      StudyEngine.do(
+        StudyEngine.participantActions.updateFlag('noCatchup', 'GetCatchup'),
+      ),
+      // else:
+      StudyEngine.participantActions.updateFlag('noCatchup', 'NoCatchup'),
+    )
+  ]
 }
