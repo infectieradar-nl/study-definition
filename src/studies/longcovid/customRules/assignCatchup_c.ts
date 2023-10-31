0d8acd4510433c0877c4b39a087f29c012d53d03
merlinvanloenen <merlin.van.loenen@rivm.nl>
2023-10-31 16:20:34 +0100

    Create assignCatchup_c.ts
    Made seperate custum rule to assing childeren catchup qeustionnaire

---
+++ src/studies/longcovid/customRules/assignCatchup_c.ts
@@ -0,0 +1,39 @@
+import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
+
+const flagKey = 'GetCatchup_c';
+
+const timestampFromStudyStart = (daysDelta: number) => {
+  return StudyEngine.timestampWithOffset(
+    {
+      days: daysDelta
+    },
+    StudyEngine.participantState.getStudyEntryTime(),
+  );
+}
+
+const assignSurveyFromStudyStart = (
+  surveyKey: string,
+  category: "prio" | "normal" | "optional",
+  startDays: number, durationDays: number
+) => {
+  return StudyEngine.participantActions.assignedSurveys.add(surveyKey, category, timestampFromStudyStart(startDays), timestampFromStudyStart(startDays + durationDays));
+}
+
+
+export const assignCatchupRetroactively_rules = {
+  name: "assignCatchup",
+  rules: [
+    StudyEngine.ifThen(
+      // IF has flag catch-up
+      StudyEngine.participantState.hasParticipantFlagKey(flagKey),
+      // ASSIGN Catch-up from study start: //we want to let it stay open for quite some time
+      assignSurveyFromStudyStart('Tstopcontinuec', "prio", 360, 360),
+      // FLAG PARTICIPANT TO BE ABLE TO FIND THEM LATER IF NEEDED:
+      StudyEngine.participantActions.updateFlag(
+        'catchupassigned',
+        StudyEngine.timestampWithOffset({ days: 0 })
+      )
+    )
+
+  ]
+}
