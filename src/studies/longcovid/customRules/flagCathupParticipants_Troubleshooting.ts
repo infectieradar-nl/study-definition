19ed0dbe7d5599e8b3cb87f4ee14bf8e98812855
merlinvanloenen <merlin.van.loenen@rivm.nl>
2023-11-08 09:04:37 +0100

    Create flagCathupParticipants_Troubleshooting.ts
    added troubleshooting flagCathupParticipants that is tested, adapted and checked. This version adds the get catchup flag to participants that have the flag 'expired'.
    
    Thereby it also double checks if in the T12.DEM.extend.FU (if a participants wanted to continue the questionnaire)

---
+++ src/studies/longcovid/customRules/flagCathupParticipants_Troubleshooting.ts
@@ -0,0 +1,45 @@
+import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
+
+
+
+export const flagcatchupParticipants_Troubleshooting_rules = {
+    name: "flagcatchupParticipants_Troubleshooting",
+    rules: [
+      StudyEngine.if(
+        // condition:
+        StudyEngine.and(
+          // If The participant missed a qeustionaire:
+          StudyEngine.participantState.hasParticipantFlagKeyAndValue('exitStatus', 'expired'),
+          // Assing the flag Get_Catch_up to participants that have T0, but not T12
+          //StudyEngine.and(
+            //StudyEngine.participantState.lastSubmissionDateOlderThan(
+             // StudyEngine.timestampWithOffset({ days: 0 }),
+              //'T0'
+            //),
+            //StudyEngine.participantState.lastSubmissionDateOlderThan(
+              //StudyEngine.timestampWithOffset({ days: 0 }),
+              //'T9c'
+            //),
+         // ),
+          // If participant didn't answer yes on T12.DEM.extend_FU //just a fail save, we dont want to send people a qeustinaire if they 
+          //said that they did not want to do the Qeustionnaire
+          StudyEngine.not(
+            StudyEngine.checkConditionForOldResponses(
+              StudyEngine.singleChoice.any(
+                'T12.DEM.extend_FU', 'nee'
+              ),
+              'any', 'T12'
+            ),
+          )
+        ),
+        // then:
+        StudyEngine.do(
+          StudyEngine.participantActions.updateFlag('noCatchup', 'GetCatchup'),
+        ),
+        // else:
+        StudyEngine.participantActions.updateFlag('noCatchup', 'NoCatchup'),
+      )
+    ]
+  }
+  
+  
