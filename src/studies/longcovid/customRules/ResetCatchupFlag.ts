2ce23842546e8ebd279b2b2a2637411a2271a4b3
merlinvanloenen <merlin.van.loenen@rivm.nl>
2023-11-27 08:41:22 +0100

    Update ResetCatchupFlag.ts
    Created a complete removal of catchup flags

--- src/studies/longcovid/customRules/ResetCatchupFlag.ts
+++ src/studies/longcovid/customRules/ResetCatchupFlag.ts
@@ -11,6 +11,12 @@ export const resetcatchupParticipants_rules = {
         // then:
         StudyEngine.do(
          StudyEngine.participantActions.removeFlag('noCatchup'),
+         StudyEngine.participantActions.removeFlag('catchupassigned'),
+         StudyEngine.participantActions.removeFlag('Gotcatchup'),
+         StudyEngine.participantActions.assignedSurveys.remove('Tstopcontinue', 'all'),
+         StudyEngine.participantActions.assignedSurveys.remove('Tstopcontinuec', 'all'),
+         StudyEngine.participantActions.messages.remove('Tstopcontinuec'),
+         StudyEngine.participantActions.messages.remove('Tstopcontinue'),
         ),
         // else:
       )
